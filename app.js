55852c62c1eeea952f20da90265e9a9d9ea47748
Sriram Jothiswaran <j.sri.sriram@gmail.com>
2018-07-01 23:10:48 +0530

    house surgeon and technician registration UI changed

--- app.js
+++ app.js
@@ -76,8 +76,7 @@ app.use (function (req, res, next) {
 
 
 // all environments
-// app.set('port', process.env.PORT || 443);
-app.set('port', process.env.PORT || 5000);
+app.set('port', process.env.PORT || 443);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.use(express.favicon());
@@ -2007,11 +2006,11 @@ app.post('/contactmailsend',function(req,res){
  });
 
 
-//  http.createServer(app).listen(80, function(){
-// 	  console.log('Express server listening on port 80 ');
-// });
+ http.createServer(app).listen(80, function(){
+	  console.log('Express server listening on port 80 ');
+});
 
- http.createServer(app).listen(4000, function(){
+ http.createServer(app).listen(3010, function(){
 	  console.log('Express server listening on port 3010 ');
 });
 
